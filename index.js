8dae8ddd7cfd9c9970c3ba5a69268b579ae5e55a
Carlo Costantino <costantinoart@gmail.com>
2019-09-30 10:08:48 -1000

    displayed results to the page

--- index.js
+++ index.js
@@ -9,6 +9,16 @@ function buildParams(params) {
     return queryParams.join("&")
 }
 
+function displayResults(responceJson) {
+  $('.js-results-list').empty();
+  $('.results-section').removeClass('hidden');
+  console.log(responceJson);
+  for (let i = 0; i < responceJson.data.length; i++) {
+    $('.js-results-list').append(`
+    <li class='results'><h2 class='park-name'>${responceJson.data[i].name}</h2><p class='description'>${responceJson.data[i].description}</p><a class='link' href='${responceJson.data[i].url}' target='_blank'>Learn More</a></li>`);
+  }
+}
+
 function getParks(query, limit) {
   const params = {
     stateCode: query,
@@ -16,8 +26,22 @@ function getParks(query, limit) {
     api_key: key,
   }
   const url = `${baseURL}?${buildParams(params)}`
-  console.log(url);
-}
+  
+  fetch(url)
+    .then(responce => {
+      if (responce.ok) {
+        $('.js-err').addClass('hidden');
+        return responce.json();
+      } else {
+        throw new Error(responce.statusText);
+      }
+    })
+    .then(responceJson => (displayResults(responceJson)))
+    .catch(err => {
+      $('.js-err').text(`Oh no, something happened ${err.message}`);
+      $('.js-err').removeClass('hidden');
+    })
+  }
 
 function watchSubmit() {
   $(".js-form").submit(event => {
